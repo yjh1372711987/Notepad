a0e6051db56553208160a69efb0c5b1fed947e04
linux_yjh <1372711987@qq.com>
2021-12-23 22:12:27 +0800

    Notepad

---
+++ Notepad.java
@@ -0,0 +1,12 @@
+
+
+public class Notepad {
+    int id;
+    String Notepad_Content;
+    String Notepad_Time;
+
+    @Override
+    public String toString() {
+        return "Notepad [id=" + id + ", Notepad_Content=" + Notepad_Content + ", Notepad_Time=" + Notepad_Time + "]";
+    }
+}
---
+++ SelectNote.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+
+@WebServlet(urlPatterns = "/SelectNote")
+public class SelectNote extends HttpServlet {
+   final static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+   final static String URL = "jdbc:mysql://180.76.151.237/linux_final";
+   final static String USER = "root";
+   final static String PASS = "1372711987Ab!";
+   final static String SQL_QURERY_ALL_NOTEPAD = "SELECT * FROM t_Notepad;";
+   Connection conn = null;
+
+   public void init() {
+      try {
+         Class.forName(JDBC_DRIVER);
+         conn = DriverManager.getConnection(URL, USER, PASS);
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
+   public void destroy() {
+      try {
+         conn.close();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
+   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      response.setContentType("application/json");
+      response.setCharacterEncoding("UTF-8");
+      PrintWriter out = response.getWriter();
+
+      List<Notepad> noteList = getAllNotepad();
+      Gson gson = new Gson();
+      String json = gson.toJson(noteList, new TypeToken<List<Notepad>>() {
+      }.getType());
+      out.println(json);
+      out.flush();
+      out.close();
+  
+}
+   private List<Notepad> getAllNotepad(){
+      List<Notepad> noteList = new ArrayList<Notepad>();
+      Statement stmt = null;
+      try {
+         stmt = conn.createStatement();
+         ResultSet rs = stmt.executeQuery(SQL_QURERY_ALL_NOTEPAD);
+         while (rs.next()) {
+            Notepad note = new Notepad();
+           note.id = rs.getInt("id");
+            note.Notepad_Content = rs.getString("Notepad_Content");
+            note.Notepad_Time = rs.getString("Notepad_Time");
+            noteList.add(note);
+         }
+         rs.close();
+         stmt.close();
+      } catch (SQLException se) {
+         se.printStackTrace();
+      } catch (Exception e) {
+         e.printStackTrace();
+      } finally {
+         try {
+            if (stmt != null)
+               stmt.close();
+         } catch (SQLException se) {
+            se.printStackTrace();
+         }
+      }
+
+      return noteList;
+   }
+}
